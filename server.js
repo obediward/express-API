69326e4d03b3c86f86db8c2ee5ffb5eba9661248
Ward obedi <obedixio@gmail.com>
2020-02-29 10:39:50 +0100

    EXPRESS-API

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const albumsData = [
+  {
+    albumId: "10",
+    artistName: "Beyoncé",
+    collectionName: "Lemonade",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
+    releaseDate: "2016-04-25T07:00:00Z",
+    primaryGenreName: "Pop",
+    url:
+      "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0"
+  },
+  {
+    albumId: "11",
+    artistName: "Beyoncé",
+    collectionName: "Dangerously In Love",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
+    releaseDate: "2003-06-24T07:00:00Z",
+    primaryGenreName: "Pop",
+    url:
+      "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0"
+  }
+];
+app.use(bodyParser.json());
+
+app.listen(3000, function() {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
+
+app.get("/albums/:albumId", function(req, res) {
+  const albumId= req.params.albumId;
+  var album = albumsData.filter(function(album) {
+    return albumId == album.albumId;
+  });
+  res.send(album);
+  console.log(album);
+});
+app.post("/albums", function(req, res) {
+  albumsData.push(req.body);
+  res.status(400).send("hello");
+});
+
+app.get("/albums", function(req, res) {
+  res.send(albumsData);
+});
+
+app.get("*", function(req, res) {
+  res.send("not defind " + req.path);
+});
